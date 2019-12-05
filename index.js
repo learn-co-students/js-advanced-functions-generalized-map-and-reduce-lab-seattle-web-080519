e9f0e06e0f56d2a54439c3f932fc3c8846ff8c74
vibraniumforge <gregm9999@gmail.com>
2019-12-05 15:50:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, callback) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+}
+
+function reduce(sourceArray, callback, startingValue) {
+  let finalValue = startingValue ? startingValue : sourceArray[0];
+  let i = startingValue ? 0 : 1;
+  for (; i < sourceArray.length; i++) {
+    // finalValue += source[i];
+    // console.log(callback);
+    finalValue = callback(sourceArray[i], finalValue);
+  }
+  return finalValue;
+}
